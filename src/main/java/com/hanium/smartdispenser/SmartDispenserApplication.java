18d74300a5dd146fdfc5e6e30beb96714a2b2670
sehyeon <Eugene8500@gmail.com>
2025-04-15 22:53:15 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/hanium/smartdispenser/SmartDispenserApplication.java
@@ -0,0 +1,13 @@
+package com.hanium.smartdispenser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDispenserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDispenserApplication.class, args);
+	}
+
+}
