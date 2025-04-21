ce3c6a95b0062a940f08d0ace7b81d56981bd060
sehyeon <Eugene8500@gmail.com>
2025-04-22 01:30:47 +0900

    refactor(common): Exception 클래스 생성자 리팩토링

--- src/main/java/com/hanium/smartdispenser/common/exception/BusinessException.java
+++ src/main/java/com/hanium/smartdispenser/common/exception/BusinessException.java
@@ -3,7 +3,7 @@ package com.hanium.smartdispenser.common.exception;
 import lombok.Getter;
 
 @Getter
-public class BusinessException extends RuntimeException {
+public abstract class BusinessException extends RuntimeException {
     private final String errCode;
 
     public BusinessException(String message, String errCode) {
@@ -11,15 +11,8 @@ public class BusinessException extends RuntimeException {
         this.errCode = errCode;
     }
 
-    public BusinessException(Throwable cause, String errCode) {
-        super(cause);
-        this.errCode = errCode;
-    }
-
     public BusinessException(String message, Throwable cause, String errCode) {
         super(message, cause);
         this.errCode = errCode;
     }
-
-
 }
--- src/main/java/com/hanium/smartdispenser/user/exception/DuplicateEmailException.java
+++ src/main/java/com/hanium/smartdispenser/user/exception/DuplicateEmailException.java
@@ -3,22 +3,14 @@ package com.hanium.smartdispenser.user.exception;
 import com.hanium.smartdispenser.common.exception.BusinessException;
 
 public class DuplicateEmailException extends BusinessException {
-    private static final String DEFAULT_MESSAGE = "이미 사용 중인 이메일입니다.";
+    private static final String DEFAULT_MESSAGE = "이미 사용 중인 이메일(%s)입니다.";
     private static final String ERROR_CODE = "USER_DUPLICATE_EMAIL";
 
-    public DuplicateEmailException() {
-        super(DEFAULT_MESSAGE,ERROR_CODE);
+    public DuplicateEmailException(String email) {
+        super(String.format(DEFAULT_MESSAGE, email), ERROR_CODE);
     }
 
-    public DuplicateEmailException(String message, String errCode) {
-        super(message, errCode);
-    }
-
-    public DuplicateEmailException(Throwable cause, String errCode) {
-        super(cause, errCode);
-    }
-
-    public DuplicateEmailException(String message, Throwable cause, String errCode) {
-        super(message, cause, errCode);
+    public DuplicateEmailException(Throwable cause, String email) {
+        super(String.format(DEFAULT_MESSAGE, email), cause, ERROR_CODE);
     }
 }
