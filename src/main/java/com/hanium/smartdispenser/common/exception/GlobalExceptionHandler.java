67e1ac17f86986ac6fbb8eb4a63e78335ff57d8f
sehyeon <Eugene8500@gmail.com>
2025-08-04 08:49:55 +0900

    feat: 비지니스 및 인프라 예외 응답 처리 추가

--- src/main/java/com/hanium/smartdispenser/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hanium/smartdispenser/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.hanium.smartdispenser.common.exception;
 
+import com.hanium.smartdispenser.user.exception.DuplicateEmailException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -27,11 +28,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(Map.of("status", 400, "errors", errors));
     }
 
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<?> handleBusinessException(BusinessException e) {
+        return ResponseEntity.badRequest().body(Map.of("status", 400, "message", e.getMessage()));
+    }
+
+    @ExceptionHandler(InfrastructureException.class)
+    public ResponseEntity<?> handleInfraException(InfrastructureException e) {
+        return ResponseEntity.badRequest().body(Map.of("status", 500, "message", e.getMessage()));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleGeneralException(Exception e) {
         log.error("[서버 예외]",e);
 
         return ResponseEntity.internalServerError().body(Map.of("status", 500, "message", "서버 내부 오류가 발생했습니다."));
-
     }
 }
